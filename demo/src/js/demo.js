4dfce3984574c2592c4f2de210032bee8ef390ed
vurghus-minar <vurghus.minar@outlook.com>
2019-06-14 13:28:43 +0400

    demo update, clean up

--- demo/src/js/demo.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $('#test').isiaFormRepeater({
-        addButton: '<div class="repeat-add-wrapper"><a data-repeat-add-btn class="repeat-add pure-button pure-button-primary" href="#">Add</a></div>',
-        removeButton: '<a data-repeat-remove-btn class="repeat-remove pure-button pure-button-primary" href="#">Remove</a>'
-    });
-    $('#test2').isiaFormRepeater();
-});
